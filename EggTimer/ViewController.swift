2d35ebb41b5fb495f1138358ea7bf2dc53c62fd6
Yevhenii Huts <yehut@smart-it.com>
2021-04-11 14:10:44 +0300

    Created main functionality.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var label: UILabel!
+    
+    //    let boilingTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let boilingTime = ["Soft": 3, "Medium": 5, "Hard": 7]
 
+    var timer = Timer()
+    var totalTime = 0
+    var secondPassed = 0
+    var player: AVAudioPlayer!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = boilingTime[hardness]!
+        progressBar.progress = 0.0
+        secondPassed = 0
+        label.text = hardness
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondPassed < totalTime {
+            secondPassed += 1
+            progressBar.progress = Float(secondPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            label.text = "Done!"
+            playFinalSound()
+        }
+    }
+    
+    func playFinalSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
